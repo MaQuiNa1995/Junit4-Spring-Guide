104e1cb548d52813425aa07f642d637dfebf14cc
Christian Muñoz Asón <cmunozas@everis.com>
2019-07-30 12:31:56 +0200

    añadido gitignore, arreglo de los test y completado de coverage

--- src/main/java/es/maquina1995/calculadoraJunit/Calculadora.java
+++ src/main/java/es/maquina1995/calculadoraJunit/Calculadora.java
@@ -2,14 +2,12 @@ package es.maquina1995.calculadoraJunit;
 
 public interface Calculadora {
 
-	public Double sumar(Double num1, Double num2);
+    public double sumar(double num1, double num2);
 
-	public Double restar(Double num1, Double num2);
+    public double restar(double num1, double num2);
 
-	public Double multiplicar(Double num1, Double num2);
+    public double multiplicar(double num1, double num2);
 
-	public Double dividir(Double dividendo, Double cociente);
-
-	public boolean comprobarNulos(Double num1, Double num2);
+    public Double dividir(Double dividendo, Double cociente);
 
 }
--- src/main/java/es/maquina1995/calculadoraJunit/CalculadoraImpl.java
+++ src/main/java/es/maquina1995/calculadoraJunit/CalculadoraImpl.java
@@ -2,47 +2,36 @@ package es.maquina1995.calculadoraJunit;
 
 public class CalculadoraImpl implements Calculadora {
 
-	
-	@Override
-	public Double sumar(Double num1, Double num2) {
+    @Override
+    public double sumar(double num1, double num2) {
 
-		return num1 + num2;
+	return num1 + num2;
+    }
 
+    @Override
+    public double restar(double num1, double num2) {
+
+	return num1 - num2;
+    }
+
+    @Override
+    public double multiplicar(double num1, double num2) {
+
+	return num1 * num2;
+    }
+
+    @Override
+    public Double dividir(Double dividendo, Double cociente) {
+
+	boolean parametrosIncorrectos = comprobarNulos(dividendo, cociente) || dividendo == 0d || cociente == 0d;
+
+	if (parametrosIncorrectos) {
+	    throw new ArithmeticException("Ninguno de los 2 parametros puede ser cero");
 	}
+	return dividendo / cociente;
+    }
 
-	@Override
-	public Double restar(Double num1, Double num2) {
-
-		return num1 - num2;
-
-	}
-
-	@Override
-	public Double multiplicar(Double num1, Double num2) {
-
-		return num1 * num2;
-
-	}
-
-	@Override
-	public Double dividir(Double dividendo, Double cociente) {
-
-		if (dividendo == 0d || cociente == 0d) {
-
-			throw new ArithmeticException("Ninguno de los 2 parametros puede ser cero");
-		}
-		return dividendo / cociente;
-
-	}
-
-	@Override
-	public boolean comprobarNulos(Double num1, Double num2) {
-		Boolean algunParametroNulo = Boolean.FALSE;
-		
-		if (num1 == null || num2 == null) {
-			algunParametroNulo = Boolean.TRUE;
-		}
-		
-		return algunParametroNulo;
-	}
+    private boolean comprobarNulos(Double num1, Double num2) {
+	return (num1 == null || num2 == null);
+    }
 }
